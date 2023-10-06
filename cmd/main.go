294d41e958216d765f3394bc614bef2f6cd7dfd6
dmitry <dmitry.tomahawk@yandex.ru>
2023-10-06 23:48:14 +0400

    implement endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,97 @@
 package main
 
 import (
+	desc "auth/pkg/user_v1"
+	"context"
 	"fmt"
-
 	"github.com/fatih/color"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
+	"time"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+func (s *server) Create(_ context.Context, req *desc.CreateUserRequest) (*desc.CreateUserResponse, error) {
+	var id int64 = 1
+
+	user := req.GetUser()
+
+	fmt.Println("Create request")
+	fmt.Println(color.GreenString("", "Id: ", id))
+	fmt.Println(color.GreenString("", "Name: ", user.Name))
+	fmt.Println(color.GreenString("", "Email: ", user.Email))
+	fmt.Println(color.GreenString("", "Role: ", user.Role))
+	fmt.Println(color.GreenString("", "Password: ", user.Password))
+	fmt.Println(color.GreenString("", "PasswordConfirm: ", user.PasswordConfirm))
+	fmt.Println("===================================================")
+
+	return &desc.CreateUserResponse{
+		Id: id,
+	}, nil
+}
+
+func (s *server) Get(_ context.Context, req *desc.GetUserRequest) (*desc.GetUserResponse, error) {
+	timestamp := timestamppb.New(time.Now())
+	user := &desc.GetUserResponse{
+		Id:        req.GetId(),
+		Name:      "Root",
+		Email:     "root@mail.ru",
+		Role:      desc.Role_ADMIN,
+		CreatedAt: timestamp,
+		UpdatedAt: timestamp,
+	}
+
+	fmt.Println("Get request")
+	fmt.Println(color.GreenString("", "Id: ", user.Id))
+	fmt.Println(color.GreenString("", "Name: ", user.Name))
+	fmt.Println(color.GreenString("", "Email: ", user.Email))
+	fmt.Println(color.GreenString("", "Role: ", user.Role))
+	fmt.Println(color.GreenString("", "CreatedAt: ", timestamp))
+	fmt.Println(color.GreenString("", "UpdatedAt: ", timestamp))
+	fmt.Println("===================================================")
+
+	return user, nil
+}
+
+func (s *server) Update(_ context.Context, req *desc.UpdateUserRequest) (*empty.Empty, error) {
+	fmt.Println("Update request")
+	fmt.Println(color.GreenString("", "Id: ", req.Id))
+	fmt.Println(color.GreenString("", "Email: ", req.Email))
+	fmt.Println(color.GreenString("", "Name: ", req.Name))
+	fmt.Println(color.GreenString("", "Role: ", req.Role))
+	fmt.Println("===================================================")
+
+	return &empty.Empty{}, nil
+}
+
+func (s *server) Delete(_ context.Context, req *desc.DeleteUserRequest) (*empty.Empty, error) {
+	fmt.Println("Delete request")
+	fmt.Println(color.GreenString("", "Id: ", req.Id))
+	fmt.Println("===================================================")
+
+	return &empty.Empty{}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,21 @@ module auth
 go 1.20
 
 require (
+	google.golang.org/grpc v1.57.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 // indirect
+)
+
+require (
 	github.com/fatih/color v1.15.0 // indirect
+	github.com/golang/protobuf v1.5.3
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 )
