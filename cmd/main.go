3ca4c9b7bec74597bb7a4e5728cec69b2f4174f5
dmitry <dmitry.tomahawk@yandex.ru>
2023-10-21 18:44:10 +0500

    implemnt db user crud

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,9 @@ import (
 	desc "auth/pkg/user_v1"
 	"context"
 	"fmt"
-	"github.com/fatih/color"
+	"github.com/Masterminds/squirrel"
 	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/jackc/pgx/v4"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -15,68 +16,138 @@ import (
 )
 
 const grpcPort = 50051
+const dbDSN = "host=localhost port=54321 dbname=course user=dev_course password=1801"
 
 type server struct {
 	desc.UnimplementedUserV1Server
 }
 
-func (s *server) Create(_ context.Context, req *desc.CreateUserRequest) (*desc.CreateUserResponse, error) {
-	var id int64 = 1
+func (s *server) Create(ctx context.Context, req *desc.CreateUserRequest) (*desc.CreateUserResponse, error) {
+	name, email, role, password, passwordConfirm :=
+		req.User.Name, req.User.Email, req.User.Role, req.User.Password, req.User.PasswordConfirm
+	if password != passwordConfirm {
+		log.Fatalf("passwords are not equal")
+	}
 
-	user := req.GetUser()
+	pool, err := pgx.Connect(ctx, dbDSN)
+	if err != nil {
+		log.Fatalf("failed to connect to database", err.Error())
+	}
 
-	fmt.Println("Create request")
-	fmt.Println(color.GreenString("", "Id: ", id))
-	fmt.Println(color.GreenString("", "Name: ", user.Name))
-	fmt.Println(color.GreenString("", "Email: ", user.Email))
-	fmt.Println(color.GreenString("", "Role: ", user.Role))
-	fmt.Println(color.GreenString("", "Password: ", user.Password))
-	fmt.Println(color.GreenString("", "PasswordConfirm: ", user.PasswordConfirm))
-	fmt.Println("===================================================")
+	insertBuilder := squirrel.Insert("\"user\"").
+		PlaceholderFormat(squirrel.Dollar).
+		Columns("name", "email", "role", "password").
+		Values(name, email, role.String(), password).
+		Suffix("RETURNING id")
+
+	query, args, err := insertBuilder.ToSql()
+	if err != nil {
+		log.Fatalf("Failed to build query")
+	}
+
+	var id int64
+	err = pool.QueryRow(ctx, query, args...).Scan(&id)
+	if err != nil {
+		log.Fatalf("Failed to make query: %v", err.Error())
+	}
 
 	return &desc.CreateUserResponse{
 		Id: id,
 	}, nil
 }
 
-func (s *server) Get(_ context.Context, req *desc.GetUserRequest) (*desc.GetUserResponse, error) {
-	timestamp := timestamppb.New(time.Now())
-	user := &desc.GetUserResponse{
-		Id:        req.GetId(),
-		Name:      "Root",
-		Email:     "root@mail.ru",
-		Role:      desc.Role_ADMIN,
-		CreatedAt: timestamp,
-		UpdatedAt: timestamp,
+func (s *server) Get(ctx context.Context, req *desc.GetUserRequest) (*desc.GetUserResponse, error) {
+	id := req.Id
+
+	pool, err := pgx.Connect(ctx, dbDSN)
+	if err != nil {
+		log.Fatalf("Failed to connect to database")
 	}
 
-	fmt.Println("Get request")
-	fmt.Println(color.GreenString("", "Id: ", user.Id))
-	fmt.Println(color.GreenString("", "Name: ", user.Name))
-	fmt.Println(color.GreenString("", "Email: ", user.Email))
-	fmt.Println(color.GreenString("", "Role: ", user.Role))
-	fmt.Println(color.GreenString("", "CreatedAt: ", timestamp))
-	fmt.Println(color.GreenString("", "UpdatedAt: ", timestamp))
-	fmt.Println("===================================================")
+	selectBuilder := squirrel.Select("id", "name", "email", "role", "created_at", "updated_at").
+		PlaceholderFormat(squirrel.Dollar).
+		From("\"user\"").
+		Where(squirrel.Eq{"id": id})
+
+	query, args, err := selectBuilder.ToSql()
+	if err != nil {
+		log.Fatalf("Failed to build query")
+	}
+
+	var userId int64
+	var userName string
+	var email string
+	var role string
+	var createdAt time.Time
+	var updatedAt *time.Time
+	err = pool.QueryRow(ctx, query, args...).Scan(&userId, &userName, &email, &role, &createdAt, &updatedAt)
+	if err != nil {
+		log.Fatalf("Failed to make query: %v", err.Error())
+	}
+
+	user := &desc.GetUserResponse{
+		Id:        userId,
+		Name:      userName,
+		Email:     email,
+		Role:      desc.Role(desc.Role_value[role]),
+		CreatedAt: timestamppb.New(createdAt),
+	}
+	if updatedAt != nil {
+		user.UpdatedAt = timestamppb.New(*updatedAt)
+	}
 
 	return user, nil
 }
 
-func (s *server) Update(_ context.Context, req *desc.UpdateUserRequest) (*empty.Empty, error) {
-	fmt.Println("Update request")
-	fmt.Println(color.GreenString("", "Id: ", req.Id))
-	fmt.Println(color.GreenString("", "Email: ", req.Email))
-	fmt.Println(color.GreenString("", "Name: ", req.Name))
-	fmt.Println(color.GreenString("", "Role: ", req.Role))
-	fmt.Println("===================================================")
+func (s *server) Update(ctx context.Context, req *desc.UpdateUserRequest) (*empty.Empty, error) {
+	id, email, name, role := req.Id, req.Email, req.Name, req.Role
+
+	pool, err := pgx.Connect(ctx, dbDSN)
+	if err != nil {
+		log.Fatalf("Failed to connect to database")
+	}
+
+	builderUpdate := squirrel.Update("\"user\"").
+		PlaceholderFormat(squirrel.Dollar).
+		Set("email", email.Value).
+		Set("name", name.Value).
+		Set("role", role.String()).
+		Set("updated_at", time.Now()).
+		Where(squirrel.Eq{"id": id})
+
+	query, args, err := builderUpdate.ToSql()
+	if err != nil {
+		log.Fatalf("failed to build query: %v", err)
+	}
+
+	_, err = pool.Exec(ctx, query, args...)
+	if err != nil {
+		log.Fatalf("failed to executed query: %v", err)
+	}
 
 	return &empty.Empty{}, nil
 }
 
-func (s *server) Delete(_ context.Context, req *desc.DeleteUserRequest) (*empty.Empty, error) {
-	fmt.Println("Delete request")
-	fmt.Println(color.GreenString("", "Id: ", req.Id))
-	fmt.Println("===================================================")
+func (s *server) Delete(ctx context.Context, req *desc.DeleteUserRequest) (*empty.Empty, error) {
+	id := req.Id
+
+	pool, err := pgx.Connect(ctx, dbDSN)
+	if err != nil {
+		log.Fatalf("failed to connect to database %v", err)
+	}
+
+	deleteBuilder := squirrel.Delete("\"user\"").
+		PlaceholderFormat(squirrel.Dollar).
+		Where(squirrel.Eq{"id": id})
+
+	query, args, err := deleteBuilder.ToSql()
+	if err != nil {
+		log.Fatalf("failed to build query %v", err)
+	}
+	_, err = pool.Exec(ctx, query, args...)
+	if err != nil {
+		log.Fatalf("failed to execute %v", err)
+	}
 
 	return &empty.Empty{}, nil
 }
@@ -91,6 +162,7 @@ func main() {
 	reflection.Register(s)
 	desc.RegisterUserV1Server(s, &server{})
 
+	fmt.Println("Server has been started")
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
